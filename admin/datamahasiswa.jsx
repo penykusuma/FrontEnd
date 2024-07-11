1c3651c4179867ab1606249083162589ae5cb3f1
penykusuma <162266637+penykusuma@users.noreply.github.com>
2024-07-11 23:50:07 +0700

    frontend 11
    get server side props

---
+++ admin/datamahasiswa.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import GetMahasiswa from "./getmahasiswa";
+
+const DataMahasiswa = ({data}) => {
+    return (
+        <>
+            <GetMahasiswa mahasiswa={data} />
+        </>
+    );
+}
+
+export const getServerSideProps = async ({query}) => {
+   const nim = query.nim
+
+   let url= `http://localhost:4000/mahasiswa`
+   if (nim) {
+    url = `http://localhost:4000/mahasiswa/${nim}`
+   }
+   const res = await axios.get(url)
+   const data = res.data
+
+    return {
+        props: {
+            data,
+        }
+    }
+}
+
+export default DataMahasiswa;
+
